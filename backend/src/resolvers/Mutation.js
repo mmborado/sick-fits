3e276967b3ae0e64618262502416b9f03d182829
Michael Borado <mmborado@gmail.com>
2019-08-22 00:56:06 -0700

    Initial Commit

---
+++ backend/src/resolvers/Mutation.js
@@ -0,0 +1,14 @@
+const Mutations = {
+  async createItem(parents, args, ctx, info) {
+    // TODO: Check if they are logged in
+
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      },}, info);
+
+    return item;
+  }
+};
+
+module.exports = Mutations;
